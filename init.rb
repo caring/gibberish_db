3daf35a153c5937946febdde8f6894f9bd30962d
Dustin Sallings <dustin@caring.com>
2008-03-28 19:22:10 -0700

    Less code to do the same work in the after filter.

--- init.rb
+++ init.rb
@@ -1,13 +1,6 @@
 require 'gibberish_db'
 require 'gibberish_db/gibberish_helper'
 
-class ActionController::Base
-
-  protected
-
-  def reset_gibberish_translations
-    Gibberish::Localize::reset_translations
-  end
+ActionController::Base.send :after_filter do
+  Gibberish::Localize::reset_translations
 end
-
-ActionController::Base.send :after_filter, :reset_gibberish_translations
